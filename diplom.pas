1e9ea2098876e666c78531eb789d23c65ba71cc3
duburlan <duburlan@764b60aa-1a20-11de-a108-cd2f117ce590>
2009-03-26 22:55:16 +0000

    Оптимизировал структуру данных. добавил комментарии

--- diplom.pas
+++ diplom.pas
@@ -7,88 +7,98 @@ const
 	n2 = 1; r2 = 5; q2 = +3;
 	pi = 3.1459;
 	pi_2 = pi/2;
-	pi10000 = 31459;
-	rh = 0.529;
+	pi10000 = 31459; {чтобы в цикле не вычислять выражение 10000*пи}
+	rh = 0.529; {радиус первой орбиты атома водорода}
 	B=43;
 type
-	TArr = array[0..n1+n2,0..4] of double;
+	TArr = array[0..n1+n2,1..4] of double; {массив из частиц}
 var
 	f:text;
 	arr:TArr;
+
+	{возведение числа х в степень n}
+	function power(x: double; n:word) : double;
+	var 
+		i:word;
+		rez:double;
+	begin
+		rez:=1;
+		for i:=1 to n do rez := rez * x;
+		power:=rez;
+	end;
+
 	function ArcSin( x:double):double;
 	begin
 		ArcSin:=Arctan(x/Sqrt(1-x*x));
 	end;
+	{первоначальный разброс частиц}
 	procedure random_array;
 	var 
 		i : word;
 		phi, tetta : double;
 	begin
-		arr[0,0] = r0; arr[0,1] = 0; arr[0,2] = 0; arr[0,3] = 0; 
-		arr[0,4] = q0; 
+		arr[0,1] := 0; arr[0,2] := 0; arr[0,3] := 0; 
+		arr[0,4] := q0; 
 		for i:=1 to n1 do 
 		begin
 			phi := random(pi10000+pi10000)/10000;
 			tetta := ArcSin(2 * random(10000)/10000 - 1) + pi_2;{}
-			arr[i,0] := r1;
-			arr[i,1] := r1*sin(tetta)*cos(phi);
-			arr[i,2] := r1*sin(tetta)*sin(phi);
-			arr[i,3] := r1*cos(tetta);
+			arr[i,1] := r1*sin(tetta)*cos(phi); {x}
+			arr[i,2] := r1*sin(tetta)*sin(phi); {y}
+			arr[i,3] := r1*cos(tetta);          {z}
 			arr[i,4] := q1;
 		end;
 		for i:=n1+1 to n1+n2 do 
 		begin
 			phi := random(pi10000+pi10000)/10000;
 			tetta := ArcSin(2 * random(10000)/10000 - 1) + pi_2;{}
-			arr[i,0] := r2;
-			arr[i,1] := r2*sin(tetta)*cos(phi);
-			arr[i,2] := r2*sin(tetta)*sin(phi);
-			arr[i,3] := r2*cos(tetta);
+			arr[i,1] := r2*sin(tetta)*cos(phi); {x}
+			arr[i,2] := r2*sin(tetta)*sin(phi); {y}
+			arr[i,3] := r2*cos(tetta);          {z}
 			arr[i,4] := q2;
 		end;
 	end;
 	
+	{сила с которой i1-я частица действует на i2-ю}
+	function Vr(i1,i2: word) : double;
+	var 
+		Rij,dx,dy,dz:double;
+	begin
+		dx := arr[i1,1]-arr[i2,1];
+		dy := arr[i1,2]-arr[i2,2];
+		dz := arr[i1,3]-arr[i2,3];
+		Rij := sqrt(dx*dx+dy*dy+dz*dz); {расстояние между частицами}
+		Vr := arr[i1,4]*arr[i2,4]/Rij+B*power(Rij,7);
+	end;
+	
+	{энергия системы}
 	function E : double;
 	var
 		i,j:word;
-		r: double;
+		rez: double;
 	begin
-		E :=0;
+		rez :=0;
+		{
 		for i :=1 to n1+n2 do
 		begin
 			writeln(f, '[', arr[i,1]:0:10,', ',
 							arr[i,2]:0:10,', ',
 							arr[i,3]:0:10,'],');
 		end;
+		{}
 		for i :=0 to n1+n2 do
 			for j:=i+1 to n1+n2 do
-				E := E + Vr(i,j);
-			end;
+				rez := rez + Vr(i,j);
+		E :=rez;
 	end;
-	function Vr(i1,i2: word) : double;
-	var 
-		Rij,dx,dy,dy:double;
-	begin
-		dx = arr[i1,1]-arr[i2,1];
-		dy = arr[i1,2]-arr[i2,2];
-		dz = arr[i1,3]-arr[i2,3];
-		Rij := sqrt(dx*dx+dy*dy+dz*dz);
-		Vr := arr[i1,4]*arr[i2,4]/Rij+B*power(Rij,7);
-	end;
-	function power(x: double, n:word) : double;
-	var 
-		i:word;
-	begin
-		for i:=1 to n do power := power * x;
-	end;
+	
 
 begin
 	assign(f, 'out.txt'); rewrite(f);
 	randomize;
 	random_array;
-	E;
+	writeln(E:0:13);
 	Vr(1,4);
-	write('hello world');
 	close(f);
-	{readln;{}
+	readln;
 end.
