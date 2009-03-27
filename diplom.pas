ecbe1747dac11643dec9ff02343585955b45960c
duburlan <duburlan@764b60aa-1a20-11de-a108-cd2f117ce590>
2009-03-27 02:55:20 +0000

    рабочая версия

--- diplom.pas
+++ diplom.pas
@@ -2,14 +2,14 @@
 {$M 16384,0,655360}
 uses crt;
 const
-	n0 = 1; r0 = 0; q0 = +4;
-	n1 = 1; r1 = 3; q1 = -2;
-	n2 = 1; r2 = 4.5; q2 = +2;
+	n0 = 1; r0 = 0; q0 = 2;
+	n1 = 8; r1 = 3; q1 = -2;
+	n2 = 0; r2 = 4.5; q2 = +2;
 	pi = 3.1459;
 	pi_2 = pi/2;
 	pi10000 = 31459; {чтобы в цикле не вычислять выражение 10000*пи}
 	rh = 0.529; {радиус первой орбиты атома водорода}
-	B=143;
+	B=42;
 	aa=2;
 	bb=4;
 	A=100;
@@ -85,10 +85,11 @@ var
 		if (i1 < i2) then
 		begin
 			if ((arr[i1,4]>0)and(arr[i2,4]>0))or((arr[i1,4]<0)and(arr[i2,4]<0)) then 
-				Vr := B*power(bb/R[i1,i2], 12) {arr[i1,4]*arr[i2,4]/R[i1,i2]{}
+				{Vr := B*power(bb/R[i1,i2], 12) {}
+				Vr :=arr[i1,4]*arr[i2,4]/R[i1,i2]{}
 			else
-				Vr := A*(power(aa/R[i1,i2], 12) - power(aa/R[i1,i2], 6))
-				{arr[i1,4]*arr[i2,4]/R[i1,i2] + B/power(R[i1,i2],9){}
+				{Vr := A*(power(aa/R[i1,i2], 12) - power(aa/R[i1,i2], 6)){}
+				Vr :=arr[i1,4]*arr[i2,4]/R[i1,i2] + B/power(R[i1,i2],9){}
 		end
 		else
 			Vr :=0;
@@ -115,7 +116,7 @@ var
 	end;
 	procedure relax;
 	var
-		d, curr_E, prev_E :double;
+		dx,dy,dz, curr_E, prev_E :double;
 		napr, k :word;
 		i:longint;
 		f:text;
@@ -126,19 +127,28 @@ var
 		begin
 			k := random(n1+n2) + 1;
 			napr := random(3) + 1;{выбираем случайное направление }
-			d := (random(10000) - 50000) / 4000000; {случайное смещение }
-			arr[k, napr] := arr[k, napr] + d;
+			dx := (random(10000) - 50000) / 4000000; {случайное смещение }
+			dy := (random(10000) - 50000) / 4000000; 
+			dz := (random(10000) - 50000) / 4000000; 
+			{arr[k, napr] := arr[k, napr] + dx; {}
+			arr[k, 1] := arr[k, 1] + dx;
+			arr[k, 2] := arr[k, 2] + dy;
+			arr[k, 3] := arr[k, 3] + dz;
+			{}
 			pereschet_R(k);
 			curr_E :=E;
 			if(((curr_E<0)and(prev_E>0))or((curr_E>0)and(prev_E<0))) then break;
 			if((((prev_E - curr_E)<0) and(prev_E>0))or(((prev_E - curr_E)>0) and(prev_E<0))) then
 			begin
-				arr[k, napr] := arr[k, napr] - d;
+				{arr[k, napr] := arr[k, napr] - d;{}
+				arr[k, 1] := arr[k, 1] - dx;
+				arr[k, 2] := arr[k, 2] - dy;
+				arr[k, 3] := arr[k, 3] - dz;{}
 				pereschet_R(k);
 			end
 			else
 				prev_E := curr_E;
-			writeln(f,E:0:0);{}
+			writeln(f,E:0:10);{}
 		end;
 		close(f);
 	end;
@@ -158,7 +168,7 @@ var
 		end;
 		writeln(fm,'[', arr[n1+n2,1]:0:10,', ',
 			arr[n1+n2,2]:0:10,', ',
-			arr[n1+n2,3]:0:10,']},axes=normal,symbol=circle,symbolsize=4); #',R[0,n1+n2]:0:4);
+			arr[n1+n2,3]:0:10,']},axes=normal,symbol=circle,symbolsize=14); #',R[0,n1+n2]:0:4);
 		close(fm);
 	end;
 begin
