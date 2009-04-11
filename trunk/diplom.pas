0547f5705540ffbe06e42dde1696ff83cfece48a
duburlan <duburlan@764b60aa-1a20-11de-a108-cd2f117ce590>
2009-04-11 01:14:06 +0000

    Рабочий вариант. Добавлена возможность выбирать функцию взаимодействия из 5-ти возможных формул

--- trunk/diplom.pas
+++ trunk/diplom.pas
@@ -22,16 +22,13 @@ type
 var
 	{n0 = 1; r0 = 0; q0 = 2;
 	n1 = 4; r1 = 3; q1 = -2;
-	n2 = 1; r2 = 4.5; q2 = +2;
-	B=10; aa=1; bb=1; A=1; {}
+	n2 = 1; r2 = 4.5; q2 = +2;{}
 	
 	q0, n1, q1, n2, q2: integer;
 	max_iter : longint;
-	h, debug, r1, r2, B, aa, bb, A, 
-	Vr0, Vl0, Ror, Rol,
-	Vr1, Vl1, Ror1, Rol1,
-	Vr2, Vl2, Ror2, Rol2:double;
-
+	h, debug, r1, r2:double; 
+	ftype:array [0..6] of byte;
+	cnst:array [1..5,1..4]of double; {константы}
 	arr: TArr;
 	R  : TRij;
 	{процедура считывает из файла константы}
@@ -39,15 +36,28 @@ var
 	var
 		f:text;
 	begin
+{
+0 1 3
+0 2 4
+1 1 4
+2 2 4
+1 2 3
+
+1 Const1*q1*q2/R +Const2*exp(R/Const3)
+2 Const1*q1*q2/rij + Const2*(R/Const3)^x (x=-7..-9);
+3 Const1*(R/Const2)^-12) - (R/Const2)^-6)
+4 Const1*(R/Const2)^-12}
 		assign(f,s); reset(f);
 		readln(f,max_iter, debug);
 		readln(f,q0);
 		readln(f,n1,r1,q1);
 		readln(f,n2,r2,q2);
-		{readln(f,A,B,aa,bb);{}
-		readln(f,Vr0,Vl0,Ror,Rol);
-		readln(f,Vr1,Vl1,Ror1,Rol1);
-		readln(f,Vr2,Vl2,Ror2,Rol2);
+		readln(f,ftype[1],cnst[1,1],cnst[1,2],cnst[1,3]);
+		readln(f,ftype[2],cnst[2,1],cnst[2,2],cnst[3,3]);
+		readln(f,ftype[3],cnst[3,1],cnst[3,2],cnst[3,3]);
+		readln(f,ftype[4],cnst[4,1],cnst[4,2],cnst[4,3]);
+		readln(f,ftype[5],cnst[5,1],cnst[5,2],cnst[5,3]);
+		ftype[0]:=0;
 		close(f);
 	end;
 	{возведение числа х в степень n}
@@ -56,14 +66,15 @@ var
 		i,m:integer;
 		rez:double;
 	begin
-		if n=0 then power:=1 {нулевая степень числа всегда равна 1}
+		(*if n=0 then power:=1 {нулевая степень числа всегда равна 1}
 		else begin
 			m := abs(n); 
 			rez:=1;
 			for i:=1 to m do rez := rez * x;
 			if n>0 then power := rez
 			else        power := 1/rez;
-		end;
+		end;*)
+		power:=exp(n*ln(x));
 	end;
 	
 	function ArcSin(x:double):double;
@@ -117,33 +128,46 @@ var
 				Rij(i,j);
 	end;
 	
-	{сила с которой i1-я частица действует на i2-ю}
-	function Vr(i1,i2: word) : double;
+	{сила с которой i-я частица действует на j-ю}
+	function Vr(i,j: word) : double;
+	var
+		ft:byte;
 	begin
-		if (i1 < i2) then
+		if (i < j) then
 		begin
-			if(i1=0) then
+			if(i=0) then
 			begin
-				if(arr[i2,4]=q1)then
-					Vr := Vr0*(power(R[i1,i2]/Ror, -12) - power(R[i1,i2]/Ror, -6)){}
+				if(arr[j,4]=q1)then
+					ft:=1 {Vr := Vr0*(power(R[i,j]/Ror, -12) - power(R[i,j]/Ror, -6)){}
 				else
-					Vr := Vl2*power(R[i1,i2]/Rol1, -12) {}
+					ft:=2; {Vr := Vl2*power(R[i,j]/Rol1, -12) {}
 			end
-			else if (arr[i1,4]=arr[i2,4]) then 
+			else if (arr[i,4]=arr[j,4]) then 
 			begin
-				if(arr[i2,4]=q1)then
-					Vr := Vl0*power(R[i1,i2]/Rol, -12) {}
+				if(arr[j,4]=q1)then
+					ft:=3{Vr := Vl0*power(R[i,j]/Rol, -12) {}
 				else
-					Vr := Vl1*power(R[i1,i2]/Rol1, -12) {}
-				{Vr :=arr[i1,4]*arr[i2,4]/R[i1,i2]{}
+					ft:=4;{Vr := Vl1*power(R[i,j]/Rol1, -12) {}
 			end
 			else 
-				Vr := Vr2*(power(R[i1,i2]/Ror2, -12) - power(R[i1,i2]/Ror2, -6)){}
-				{Vr :=arr[i1,4]*arr[i2,4]/R[i1,i2] + B/power(R[i1,i2],9){}
-			{Vr := arr[i1,4]*arr[i2,4]/R[i1,i2] + B*power(R[i1,i2],-12);{}
+				ft:=5;{Vr := Vr2*(power(R[i,j]/Ror2, -12) - power(R[i,j]/Ror2, -6)){}
+				{Vr :=arr[i,4]*arr[j,4]/R[i,j] + B/power(R[i,j],9){}
+			{Vr := arr[i,4]*arr[j,4]/R[i,j] + B*power(R[i,j],-12);{}
 		end
 		else
-			Vr :=0;
+			ft:=0;{Vr :=0;{}
+		
+		case ftype[ft] of
+			1: Vr := cnst[ft,1] * arr[i,4] * arr[j,4] / R[i,j] + 
+					cnst[ft,2] * exp(R[i,j] / cnst[ft,3]);
+			2: Vr := cnst[ft,1]*arr[i,4]*arr[j,4]/R[i,j] + 
+					cnst[ft,2]*power(R[i,j]/cnst[ft,3], -8);
+			3: Vr := cnst[ft,1]*(power(R[i,j]/cnst[ft,2],-12) -
+					power(R[i,j]/cnst[ft,2], -6));
+			4: Vr := cnst[ft,1]*power(R[i,j]/cnst[ft,2],-12);
+			5: Vr := cnst[ft,1]*arr[i,4]*arr[j,4]/R[i,j] + cnst[ft,2]*power(R[i,j]/cnst[ft,3],-12);
+			else Vr := 0;
+		end;{}
 	end;
 	
 	{энергия системы}
@@ -176,12 +200,12 @@ var
 		i, j, check:longint;
 		f:text;
 	begin
-		h:=0.4;
+		h:=0.3;
 		check := 0;
-		if (debug=1) then 
+		{if (debug=1) then 
 		begin 
 			assign(f, 'out.txt'); rewrite(f); 
-		end;
+		end;{}
 		
 		prev_E := E; {предыдущее значении энергии. Обратите внимание E - 
 		это не переменная, это процедура}
@@ -215,18 +239,18 @@ var
 			end
 			else begin
 				prev_E := curr_E;
-				if (debug=1) then writeln(f,curr_E:0:10);
+				{if (debug=1) then writeln(f,curr_E:0:10);{}
 				inc(check);
 				if(check=9)or(check=90)or(check=200)or(check=500)or(check=1000)
 					then h:=h/2;
 			end;
 		end;
 		
-		if (debug=1) then
+		{if (debug=1) then
 		begin
 			writeln(f,max_iter,' ',check, ' ',(check/max_iter*100):0:8,'%');
 			close(f);
-		end;
+		end;{}
 	end;
 	{процедура печати координат частиц в специальном формате Maple}
 	procedure maple_out;
@@ -255,8 +279,7 @@ var
 				write(fm, R[i,j]:8:5,' |');
 			writeln(fm);
 		end;
-		writeln(fm,'#',Vr0:0:3,' ',Vl0:0:3,' ',Ror:0:3,' ',Rol:0:3);
-		
+
 		close(fm);
 	end;
 begin
